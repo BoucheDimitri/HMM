Fixed shape broadcasting bugs, runs

ResampleMove.py
@@ -17,24 +17,24 @@ def logprop_pz(z, y, x, eta):
 
 
 def c_matrix(t):
-    c = np.zeros((t+1, t+1))
-    np.fill_diagonal(c, 2*np.ones((t+1, )))
+    c = np.zeros((t, t))
+    np.fill_diagonal(c, 2*np.ones((t, )))
     c[0, 0] = 1
-    c[t, t] = 1
-    diaginf = np.eye(t)
-    diaginf = np.insert(diaginf, t, 0, axis=1)
+    c[t-1, t-1] = 1
+    diaginf = np.eye(t-1)
+    diaginf = np.insert(diaginf, t-1, 0, axis=1)
     diaginf = np.insert(diaginf, 0, 0, axis=0)
-    diagsup = np.eye(t)
+    diagsup = np.eye(t-1)
     diagsup = np.insert(diagsup, 0, 0, axis=1)
-    diagsup = np.insert(diagsup, t, 0, axis=0)
+    diagsup = np.insert(diagsup, t-1, 0, axis=0)
     c -= diaginf + diagsup
     return c
 
 
 def logprop_prior_ppf(xps, yps, tau):
     k = xps.shape[0]
-    cxps = xps.reshape((k + 1, 1))
-    cyps = yps.reshape((k + 1, 1))
+    cxps = xps.reshape((k, 1))
+    cyps = yps.reshape((k, 1))
     cmat = c_matrix(k)
     xterm = -0.5 * tau * np.dot(np.dot(cxps.T,
                                        cmat),
@@ -61,11 +61,11 @@ def one_logweight(particle, zk, tau, eta):
     x, y = particle_to_xyvecs(particle)
     xps = x[1:]
     yps = y[1:]
-    pikminus1 = logprop_prior_ppf(xps[:, k-1],
-                                  yps[:, k-1],
+    pikminus1 = logprop_prior_ppf(xps[:k-1],
+                                  yps[:k-1],
                                   tau)
-    pik = logprop_prior_ppf(xps[:, k],
-                            yps[:, k],
+    pik = logprop_prior_ppf(xps[:k],
+                            yps[:k],
                             tau)
     xk = np.sum(x)
     yk = np.sum(y)
@@ -76,7 +76,7 @@ def one_logweight(particle, zk, tau, eta):
     lpypk = logprop_gauss_ppf(yps[-1],
                              yps[-2],
                              math.sqrt(1 / tau))
-    return pik - pikminus1 + lpzk - lpxpk - lpypk
+    return np.float(pik - pikminus1 + lpzk - lpxpk - lpypk)
 
 
 def initialization(mprior, stdprior, N):
@@ -84,3 +84,34 @@ def initialization(mprior, stdprior, N):
     y0s = np.random.normal(mprior[1], stdprior[1], N)
     xp0s = np.random.normal(mprior[2], stdprior[2], N)
     yp0s = np.random.normal(mprior[3], stdprior[3], N)
+
+
+
+#eta = std of noise in measurement
+eta = 0.005
+#tau is such that math.sqrt(1/tau) is the std for speeds
+tau = 1000000
+#N is the number of particles
+N = 1000
+#T is the number of periods
+T = 100
+#Initial conditions
+x0 = 3
+y0 = 5
+xp0 = 0.002
+yp0 = -0.013
+#noise on initial position
+mux = 0.01
+muy = -0.4
+mprior = [x0 + mux, y0 + muy, 0.002, -0.013]
+stdprior = [0.04, 0.4, 0.003, 0.003]
+data = datagenerator.loc_data(x0, y0, xp0, yp0, T, tau, eta)
+
+toyparticle = np.array([data.loc[0, "x"]])
+toyparticle = np.append(toyparticle, np.array([data.loc[1:20, "xp"]]))
+toyparticle = np.append(toyparticle, np.array([data.loc[0, "y"]]))
+toyparticle = np.append(toyparticle, np.array([data.loc[1:20, "yp"]]))
+
+lw = one_logweight(toyparticle, data.loc[20, "z"], tau, eta)
+
+x, y = particle_to_xyvecs(toyparticle)